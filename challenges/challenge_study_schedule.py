bca513b5859a8858c7dc4aea4b4756982921f5e8
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-05-25 16:10:28 -0300

    feat: req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui"""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    countador = 0
+    for time_entrad, time_saida in permanence_period:
+        if not isinstance(time_entrad, int) or not isinstance(time_saida, int):
+            return None
+        if time_entrad <= target_time <= time_saida:
+            countador += 1
+
+    return countador
