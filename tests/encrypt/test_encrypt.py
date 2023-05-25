6230910d26b3f5c48db128d57a27e69ff66bd6a4
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-05-25 16:37:41 -0300

    feat: req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(
+            '1', 'message') or encrypt_message(
+                1, 1) or encrypt_message(1, 'message')
+
+    assert encrypt_message('message', -1) == 'egassem'
+    assert encrypt_message('message', 1) == 'm_egasse'
+    assert encrypt_message('message', 2) == 'egass_em'
