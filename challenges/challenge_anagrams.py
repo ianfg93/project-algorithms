c4f2816454055384b97d742debd99d8c8226252e
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-05-26 15:50:36 -0300

    feat: req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,38 @@
+def merge_sort(string):
+    if len(string) <= 1:
+        return string
+
+    mid = len(string) // 2
+    left = merge_sort(string[:mid])
+    right = merge_sort(string[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    new_string = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            new_string.append(left[left_index])
+            left_index += 1
+        else:
+            new_string.append(right[right_index])
+            right_index += 1
+
+    new_string.extend(left[left_index:])
+    new_string.extend(right[right_index:])
+
+    return new_string
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = "".join(merge_sort(list(first_string.lower())))
+    second_string = "".join(merge_sort(list(second_string.lower())))
+
+    if first_string == "" and second_string == "":
+        return (first_string, second_string, False)
+    if first_string == second_string:
+        return (first_string, second_string, True)
+    else:
+        return (first_string, second_string, False)
